6c4cf0f0b145480c7958e58589d6266215a4da21
miinjoo <minjoo9903@ewhain.net>
2023-04-18 00:04:14 +0900

    feat: 이미지 업로드 기능 구현

--- static/js/content.js
+++ static/js/content.js
@@ -1,18 +1,64 @@
-const Content = document.getElementById("contentabout");
-const Title = document.getElementById("contenttitle");
-const Url = document.getElementById("contenturl");
-const Detail = document.getElementById("contentdetail");
+function DropFile(dropAreaId, fileListId) {
+  let dropArea = document.getElementById(dropAreaId);
+  let fileList = document.getElementById(fileListId);
 
-function handleContent(event) {
-  event.preventDefault();
-  const title = Title.value;
-  const url = Url.value;
-  const detail = Detail.value;
-  console.log(title);
-  console.log(url);
-  console.log(detail);
-  toastr.success("게시 완료되었습니다");
-  window.location.href = "home";
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function highlight(e) {
+    preventDefaults(e);
+    dropArea.classList.add("highlight");
+  }
+
+  function unhighlight(e) {
+    preventDefaults(e);
+    dropArea.classList.remove("highlight");
+  }
+
+  function handleDrop(e) {
+    unhighlight(e);
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    handleFiles(files);
+
+    const fileList = document.getElementById(fileListId);
+    if (fileList) {
+      fileList.scrollTo({ top: fileList.scrollHeight });
+    }
+  }
+
+  function handleFiles(files) {
+    files = [...files];
+    // files.forEach(uploadFile);
+    files.forEach(previewFile);
+  }
+
+  function previewFile(file) {
+    console.log(file);
+    renderFile(file);
+  }
+
+  function renderFile(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function () {
+      let img = dropArea.getElementsByClassName("preview")[0];
+      img.src = reader.result;
+      img.style.display = "block";
+    };
+  }
+
+  dropArea.addEventListener("dragenter", highlight, false);
+  dropArea.addEventListener("dragover", highlight, false);
+  dropArea.addEventListener("dragleave", unhighlight, false);
+  dropArea.addEventListener("drop", handleDrop, false);
+
+  return {
+    handleFiles,
+  };
 }
 
-Content.addEventListener("submit", handleContent);
+const dropFile = new DropFile("drop-file", "files");
