072b698cf34defa29c974b8702f3e1d68a458c33
miinjoo <minjoo9903@ewhain.net>
2023-05-17 14:32:19 +0900

    feat: 제품 페이지 이벤트 추가

---
+++ static/js/cate.js
@@ -0,0 +1,277 @@
+const acneSkinCare = [
+  { id: 1, name: "다이브인 저분자 히알루론산 토너", company: "토리든" },
+  { id: 2, name: "아쿠아 오아시스 토너", company: "에스네이처" },
+  { id: 3, name: "다이브인 저분자 히알루론산 수딩 크림", company: "토리든" },
+  { id: 4, name: "다이브인 저분자 히알루론산 세럼", company: "토리든" },
+  { id: 5, name: "아토베리어 365 크림", company: "에스트라" },
+  { id: 6, name: "1025 독도 토너", company: "라운드랩" },
+  { id: 7, name: "다이브인 저분자 히알루론산 크림", company: "리얼베리어" },
+  {
+    id: 8,
+    name: "아쿠아 콜라겐 펩타이드 트리플 젤 에센스",
+    company: "에스네이처",
+  },
+];
+
+const acneMask = [
+  { id: 1, name: "캐롯 카로틴 카밍 워터 패드", company: "스킨푸드" },
+  { id: 2, name: "다이브인 저분자 히알루론산 마스크팩", company: "토리든" },
+  { id: 3, name: "파워10 감초줄렌 젤리패드", company: "잇츠스킨" },
+  { id: 4, name: "마스크팩", company: "다자연" },
+  { id: 5, name: "모이스처 밸런싱 마스크", company: "백아율" },
+];
+
+const drySkinCare = [
+  { id: 1, name: "아토베리어 365 크림", company: "에스트라" },
+  { id: 2, name: "화이트 트러플 퍼스트 스프레이 세럼", company: "달바" },
+  { id: 3, name: "1025 독도 토너", company: "라운드랩" },
+  { id: 4, name: "세라마이드 아토 로션", company: "일리윤" },
+  { id: 5, name: "아쿠아 스쿠알란 수분크림", company: "에스네이처" },
+  { id: 6, name: "레드 블레미쉬 클리어 수딩 크림", company: "닥터지" },
+  { id: 7, name: "익스트림 크림", company: "리얼베리어" },
+  { id: 8, name: "화이트 트러플 더블 세럼 앤 크림", company: "달바" },
+];
+
+const dryMask = [
+  { id: 1, name: "다이브인 저분자 히알루론산 마스크팩", company: "토리든" },
+  { id: 2, name: "캐롯 카로틴 카밍 워터 패드", company: "스킨푸드" },
+  { id: 3, name: "파워10 감초줄렌 젤리패드", company: "잇츠스킨" },
+  { id: 4, name: "모델링마스크 컵팩", company: "린제이" },
+  { id: 5, name: "마스크팩", company: "다자연" },
+];
+
+const oilySkinCare = [
+  { id: 1, name: "1025 독도 토너", company: "라운드랩" },
+  { id: 2, name: "레드 블레미쉬 클리어 수딩 크림", company: "닥터지" },
+  { id: 3, name: "다이브인 저분자 히알루론산 세럼", company: "토리든" },
+  { id: 4, name: "다이브인 저분자 히알루론산 토너", company: "토리든" },
+  { id: 5, name: "다이브인 저분자 히알루론산 수딩크림", company: "토리든" },
+  { id: 6, name: "다이브인 저분자 히알루론산 크림", company: "토리든" },
+  { id: 7, name: "아쿠아 스쿠알란 수분크림", company: "에스네이처" },
+  { id: 8, name: "아쿠아 오아시스 토너", company: "에스네이처" },
+];
+
+const oilyMask = [
+  { id: 1, name: "다이브인 저분자 히알루론산 마스크팩", company: "토리든" },
+  { id: 2, name: "캐롯 카로틴 카밍 워터 패드", company: "스킨푸드" },
+  { id: 3, name: "파워10 감초줄렌 젤리패드", company: "잇츠스킨" },
+  { id: 4, name: "티트리 트러블 패드", company: "메디힐" },
+  { id: 5, name: "마스크팩", company: "다자연" },
+];
+
+const popularCosmetic = [
+  { id: 1, name: "다이브인 저분자 히알루론산 마스크팩", company: "토리든" },
+  { id: 2, name: "세라마이드 아토 로션", company: "일리윤" },
+  { id: 3, name: "다이브인 저분자 히알루론산 토너", company: "토리든" },
+  { id: 4, name: "센시비오 H2O", company: "바이오더마" },
+  { id: 5, name: "아토베리어 365 크림", company: "에스트라" },
+  { id: 6, name: "녹두 밸런싱 토너", company: "비플레인" },
+];
+
+const makeUp = [
+  {
+    id: 1,
+    name: "웨이크메이크 워터 벨벳 커버 파운데이션",
+    company: "웨이크메이크",
+  },
+  {
+    id: 2,
+    name: "아워클래스 팬덤 볼류마이징 글로시 밤",
+    company: "아워글래스",
+  },
+  { id: 3, name: "어바웃톤 블러 파우더 팩트 기획", company: "아버웃톤" },
+  { id: 4, name: "헤라 블랙 쿠션", company: "헤라" },
+  { id: 5, name: "퓌 쿠션 글래스 리필 기획", company: "퓌" },
+  { id: 6, name: "데이지크 섀도우 팔레트", company: "데이지크" },
+  { id: 7, name: "라네즈 네오 쿠션 매트 15g", company: "라네즈" },
+  { id: 8, name: "클리오 킬커버 더뉴 파운웨어 쿠션 기획", company: "클리오" },
+  { id: 9, name: "바닐라코 프라이머 피니쉬 파우더 12g", company: "바닐라코" },
+];
+
+const sunCare = [
+  { id: 1, name: "순정 디렉터 수분 선크림", company: "에뛰드" },
+  { id: 2, name: "자작나무 수분 선크림", company: "라운드랩" },
+  { id: 3, name: "셀퓨전씨 레이저 썬스크린 스페셜", company: "셀퓨전씨" },
+  {
+    id: 4,
+    name: "더마UV365 장벽수분 무기자차 선크림 40ml",
+    company: "에스크라",
+  },
+  { id: 5, name: "산소수페이스앤바디보송선스틱", company: "식물나라" },
+  { id: 6, name: "닥터지 그린 마일드업 선 플러스 40ml", company: "닥터지" },
+  { id: 7, name: "AHC 마스터즈 엔어 리치 선스틱 듀오", company: "AHC" },
+  { id: 8, name: "아떼 비건 릴리프 선에센스 70ml", company: "아떼" },
+];
+
+const bodyCare = [
+  { id: 1, name: "세라마이드 아토 로션 시나모롤 기획", company: "일리윤" },
+  { id: 2, name: "낫포유 클리어 바디 미스트 200ml", company: "낫포유" },
+  {
+    id: 3,
+    name: "일리윤 프레쉬 모이스춰스크럽 폼폼푸린 기획 400ml",
+    company: "일",
+  },
+  { id: 4, name: "바이오더마 아토덤 울크라 크림", company: "바이오더마" },
+  {
+    id: 5,
+    name: "쉬크 인튜이션 잔망루피에디션 버라이어티 기획 2종",
+    company: "쉬크",
+  },
+  { id: 6, name: "일리윤 세라마이드 아토 로션 600ml", company: "일리윤" },
+  { id: 7, name: "트리헛시어 슈가 스크럽 510g", company: "트리헛" },
+];
+
+const category = document.querySelector("#category_wrapper");
+
+const template = `
+  <div class="wrapper">
+      <div class="image"></div>
+      <div class="info">
+          <div class="company"></div>
+          <div class="name"></div>
+      </div>
+  </div>
+  `;
+
+category.addEventListener("click", (e) => {
+  if (e.target.closest("figure") === null) return;
+  allEvent(e.target.closest("figure"));
+  skinEvent(e.target.closest("figure"));
+  maskEvent(e.target.closest("figure"));
+  makeupEvent(e.target.closest("figure"));
+  suncareEvent(e.target.closest("figure"));
+  bodyEvent(e.target.closest("figure"));
+});
+
+const allEvent = (target) => {
+  if (!target.classList.contains("all")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const allItem = [
+    ...acneMask,
+    ...acneSkinCare,
+    ...dryMask,
+    ...drySkinCare,
+    ...oilyMask,
+  ];
+  const optionBox = `${allItem
+    .map((option) => {
+      return `<div class="wrapper">
+                  <div class="image"></div>
+                  <div class="info">
+                    <div class="company">${option.company}</div>
+                    <div class="name">${option.name}</div>
+                  </div>
+                </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
+
+const skinEvent = (target) => {
+  if (!target.classList.contains("skin")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const optionBox = `${acneSkinCare
+    .map((option) => {
+      return `<div class="wrapper">
+                    <div class="image"></div>
+                    <div class="info">
+                      <div class="company">${option.company}</div>
+                      <div class="name">${option.name}</div>
+                    </div>
+                  </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
+
+const maskEvent = (target) => {
+  if (!target.classList.contains("mask")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const optionBox = `${acneMask
+    .map((option) => {
+      return `<div class="wrapper">
+                      <div class="image"></div>
+                      <div class="info">
+                        <div class="company">${option.company}</div>
+                        <div class="name">${option.name}</div>
+                      </div>
+                    </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
+
+const makeupEvent = (target) => {
+  if (!target.classList.contains("makeup")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const optionBox = `${makeUp
+    .map((option) => {
+      return `<div class="wrapper">
+                        <div class="image"></div>
+                        <div class="info">
+                          <div class="company">${option.company}</div>
+                          <div class="name">${option.name}</div>
+                        </div>
+                      </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
+
+const suncareEvent = (target) => {
+  if (!target.classList.contains("suncare")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const optionBox = `${sunCare
+    .map((option) => {
+      return `<div class="wrapper">
+                        <div class="image"></div>
+                        <div class="info">
+                          <div class="company">${option.company}</div>
+                          <div class="name">${option.name}</div>
+                        </div>
+                      </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
+
+const bodyEvent = (target) => {
+  if (!target.classList.contains("body")) return;
+  if (document.querySelector(".wrapper") !== null) {
+    document.querySelector(".itemList").innerHTML = "";
+  }
+  const optionBox = `${bodyCare
+    .map((option) => {
+      return `<div class="wrapper">
+                        <div class="image"></div>
+                        <div class="info">
+                          <div class="company">${option.company}</div>
+                          <div class="name">${option.name}</div>
+                        </div>
+                      </div>`;
+    })
+    .join("")}`;
+  document
+    .querySelector(".itemList")
+    .insertAdjacentHTML("afterbegin", optionBox);
+};
