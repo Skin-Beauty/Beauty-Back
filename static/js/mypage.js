9b974fa1df9008374e5c34a7295c3dc604c7e5ad
miinjoo <minjoo9903@ewhain.net>
2023-06-15 00:27:35 +0900

    fix: 마이페이지 이미지 업로드 방식 수정

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -3,6 +3,8 @@ const diagnoseBtn = document.querySelector(".btn");
 diagnoseBtn.addEventListener("click", (e) => {
   const uploadBox = document.querySelector(".contents");
   const container = document.querySelector(".type_wrapper");
-  container.classList.remove("hidden");
   uploadBox.classList.add("hidden");
+  setTimeout(() => container.classList.remove("hidden"), 2000);
+  
+  
 });
